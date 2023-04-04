58cd4c740ce886cb639ee93a346b95245dec8c17
Gregory Dutton <greg.dutton@gmail.com>
2023-04-04 08:19:20 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+var messages = [];
+
+function writeCards(name, event) {
+  for (let i = 0; i < name.length; i++) {
+    messages[i] = (`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+
+writeCards(name, event);
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(5);
